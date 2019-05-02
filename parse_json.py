2e24b3588b84c4ecfa2f69394f31d0d73f7c88c4
Thomas Mosley <tmosley032000@me.com>
2019-05-01 17:02:56 -0700

    initail commit

---
+++ parse_json.py
@@ -0,0 +1,35 @@
+import json
+import pdb
+
+
+
+f = open("foo.txt")
+#opening bracket
+j = "{"
+
+for line in f:
+  if "Number" in line:
+     x = line.split(":")
+     j += '"Threads %s": { "SQL Statistics": { "queries": {' %(x[1].strip())
+
+  elif any(item in line for item in ['read:', 'write:', 'other:','transactions:','queries:','total time:','min:','avg:','max','95th percentile:''reconnects:','events (avg/stddev):']): 
+     x = line.split(":")
+     j += '"%s": "%s",' %(x[0].strip(),x[1].strip())
+
+  elif "total:" in line:
+     x = line.split(":")
+     j += '"%s": "%s" }},' %(x[0].strip(),x[1].strip()) 
+
+  elif any(item in line for item in ['General statistics:','Latency (ms):','Threads fairness:']): 
+     j += '"%s": {' %(line.strip())
+
+  elif any(item in line for item in ['total number of events:','sum:']):
+     x = line.split(":")
+     j += '"%s": "%s" },' %(x[0].strip(), x[1].strip())
+
+  elif "execution time (avg/stddev):" in line:
+     x = line.split(":")
+     j+= '"%s": "%s" }},'  %(x[0].strip(), x[1].strip())
+j = j[:-1]
+j += "}"
+
